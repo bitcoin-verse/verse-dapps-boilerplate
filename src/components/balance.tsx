5b72a9a597983fc5d3d32f5bb0cf3ccd3cb4a820
Julie <julie@bitcoin.com>
2025-05-26 11:36:42 +0900

    update verse icon

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -4,8 +4,8 @@ import { Box } from "@/components/ui/box";
 import { CONTRACT_ADDRESSES } from "../utils/constants";
 import { mainnet } from "viem/chains";
 import { erc20Abi, formatUnits } from "viem";
-import VerseLogo from "./ui/svg/verse-logo";
 import { useEffect } from "react";
+import VerseIcon from "./ui/svg/verse-icon";
 
 export default function Balance(props: React.HTMLAttributes<HTMLDivElement>) {
   const { isConnected, chainId, address } = useAccount();
@@ -29,7 +29,7 @@ export default function Balance(props: React.HTMLAttributes<HTMLDivElement>) {
           Verse Balance
         </h1>
         <div className="mt-4 flex gap-4">
-          {formatUnits(data ?? 0n, 18)} <VerseLogo size="20" />
+          {formatUnits(data ?? 0n, 18)} <VerseIcon size={24}/>
         </div>
       </Box>
     )
